63754d4051c70ab233c8579ade7ae8b793ab0b80
Mohaamad <rezasarwary12@gmail.com>
2026-06-29 15:03:34 +0430

    commit 2

--- emotion_detection.py
+++ emotion_detection.py
@@ -5,6 +5,6 @@ def emotion_detector(text_to_analyze ):
     headers= {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     json_data= { "raw_document": { "text": text_to_analyze } }
 
-    response= requests.post(url=url,headers=headers,data=json_data)
+    response= requests.post(url=url,headers=headers,json=json_data)
 
     return response.text
