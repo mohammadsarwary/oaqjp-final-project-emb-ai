ea32933132dbc076b072ef432cabab8ad14a3881
Mohaamad <rezasarwary12@gmail.com>
2026-06-30 23:34:07 +0430

    commit 6

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -13,4 +13,20 @@ class TestEmotionDetecton(unittest.TestCase):
     def test_emotion_detection(self):
 
         result_1=emotion_detector("I am glad this happened")
-        self.assertEqual(result_1,)
+        self.assertEqual(result_1,"joy")
+
+        result_2=emotion_detector("I am really mad about this")
+        self.assertEqual(result_2,"anger")
+
+        result_3=emotion_detector("I feel disgusted just hearing about this")
+        self.assertEqual(result_3,"disgust")
+
+        result_4=emotion_detector("I am so sad about this")
+        self.assertEqual(result_4,"sadness")
+
+        result_5=emotion_detector("I am really afraid that this will happen")
+        self.assertEqual(result_5,"fear")
+
+
+if __name__ == "main":
+    unittest.main()
