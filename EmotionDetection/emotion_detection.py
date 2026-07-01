94edc8edad09c4ee9aeb7fb800e6cc33acecfeca
Mohaamad <rezasarwary12@gmail.com>
2026-07-01 14:54:00 +0430

    commit 14

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,6 +7,17 @@ def emotion_detector(text_to_analyze):
     json_data= { "raw_document": { "text": text_to_analyze } }
 
     response= requests.post(url=url,headers=headers,json=json_data)
+
+    if response.status_code == 400:
+        return {
+                "anger": None,
+                "disgust": None,
+                "fear": None,
+                "joy": None,
+                "sadness": None,
+                "dominant_emotion": None
+            }
+
     response_text=json.loads(response.text)
     emotions = response_text["emotionPredictions"][0]["emotion"]
 
